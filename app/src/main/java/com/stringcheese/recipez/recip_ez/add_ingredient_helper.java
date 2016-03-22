513913b62432c558ada3a56a0607a1b74b04a221
Kevin Chiang <kevinc9364@gmail.com>
2016-03-21 22:27:17 -0400

    more interface stuff

---
+++ app/src/main/java/com/stringcheese/recipez/recip_ez/add_ingredient_helper.java
@@ -0,0 +1,33 @@
+package com.stringcheese.recipez.recip_ez;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class add_ingredient_helper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setContentView(R.layout.activity_add_ingredient_helper);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
+
